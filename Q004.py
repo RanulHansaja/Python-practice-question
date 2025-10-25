4669e36a0c6dd697356d93be66e12a690f9e0151
Ranul Hansaja <ranulh6@gmail.com>
2025-10-25 20:43:58 +0530

    05 Question

---
+++ Q004.py
@@ -0,0 +1,7 @@
+print("Revers a String")
+
+text = input("Entert the String : ")
+
+revers_text = text[::-1]
+
+print("Revers text = ",revers_text)
---
+++ Q005.py
@@ -0,0 +1,12 @@
+print("Check Palindrome")
+
+text = input("Enter the String :")
+
+reers_text = text[::-1]
+
+if (text == revers_text):
+    print("Palindrome text")
+    
+else:
+    print("Not Palindrome text")
+
