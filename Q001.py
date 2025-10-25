c2b9420f8b1d2800f435d1611ac6f77a4e594e58
Ranul Hansaja <ranulh6@gmail.com>
2025-10-25 12:07:36 +0530

    01 Quesion

---
+++ Q001.py
@@ -0,0 +1,19 @@
+print("1. Find the Largest of Three Numbers")
+
+num01 = float(input("Enter the first Number :"))
+num02 = float(input("Enter the second Numner :"))
+num03 = float(input("Enter the Third Number :"))
+
+if num01 > num02 and num01  > num03:
+    print(f"\n{num01} is the largest number ")
+    
+elif num02 > num02 and num02 > num03:
+    print(f"\n{num02} is the largest number ")
+    
+else:
+    print(f"\n{num03} is the largest number ")
+    
+    
+
+        
+    
