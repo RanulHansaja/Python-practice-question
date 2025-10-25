c299a21f70d722375cee97a06001bd8082d671b4
Ranul Hansaja <ranulh6@gmail.com>
2025-10-25 12:18:24 +0530

    02 Question

---
+++ Q002.py
@@ -0,0 +1,9 @@
+print("2. Sum of Natural Numbers Using Loop\n")
+
+sum =0
+num = int(input("Enter the Number :"))
+
+for i in range(0 , num+1 , 1):
+    sum += i
+    
+print("Sum of the NUmbers = ",sum)
